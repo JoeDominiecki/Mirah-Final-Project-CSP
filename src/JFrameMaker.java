ecbf9be55477e4a3c491f65d9d96936e4f54fe2f
20jdominiecki <joedom301@gmail.com>
2019-05-21 13:19:38 -0400

    JFrameMaker works, added counterclass with a static score variable to make everything easier. Runner class is not that important

--- src/JFrameMaker.java
+++ src/JFrameMaker.java
@@ -1,56 +1,43 @@
+import sun.security.ssl.Debug;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class JFrameMaker {
-    static int pointsToAdd = 0;
-    static JFrame f = new JFrame("Finding Agent Mirah");
 
-    public JFrameMaker() {
+public class JFrameMaker extends JFrame implements ActionListener {
+    static int x;
+    static int[] pointsArray;
 
-    }
+    public JFrameMaker(String[] s, int[] numPoints){
 
-    public int returnMethod(String[] s, int[] numPoints){
-
+        this.setTitle("Finding Agent Mirah");
+        pointsArray = numPoints;
         Dimension SCREENSIZE = Toolkit.getDefaultToolkit().getScreenSize();
         int SCREENWIDTH = SCREENSIZE.width;
         int SCREENHEIGHT = SCREENSIZE.height;
 
-
-        JButton button1 = new JButton(s[1]);
+        JButton button1 = new JButton(s[0]);
         button1.setPreferredSize(new Dimension(200,200));
-        button1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
-                 pointsToAdd+=numPoints[1];
-            }
-        });
-        JButton button2 = new JButton(s[2]);
+        button1.addActionListener(this);
+        button1.setActionCommand("b1");
+
+        JButton button2 = new JButton(s[1]);
         button2.setPreferredSize(new Dimension(200,200));
-        button2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
-                pointsToAdd+=numPoints[2];
-            }
-        });
-        JButton button3 = new JButton(s[3]);
+        button2.addActionListener(this);
+        button2.setActionCommand("b2");
+
+        JButton button3 = new JButton(s[2]);
         button3.setPreferredSize(new Dimension(200,200));
-        button3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
-                pointsToAdd+=numPoints[3];
-            }
-        });
+        button3.addActionListener(this);
+        button3.setActionCommand("b3");
+
         JButton quitButton = new JButton("Quit");
         quitButton.setPreferredSize(new Dimension(200,200));
-        quitButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
-                f.setEnabled(false);
-                return;
-            }
-        });
-        JLabel question = new JLabel(s[0],SwingConstants.CENTER);
+        quitButton.addActionListener(this);
+        quitButton.setActionCommand("quit");
+
+        JLabel question = new JLabel(s[3],SwingConstants.CENTER);
         question.setPreferredSize(new Dimension(600,200));
 
         JPanel p = new JPanel(new BorderLayout());
@@ -62,21 +49,42 @@ public class JFrameMaker {
         p.add(quitButton, BorderLayout.SOUTH);
 
 
-        f.setContentPane(p);
-        f.setAlwaysOnTop(true);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.pack();
-        f.setLocation((SCREENWIDTH/2)-f.getWidth()/2, (SCREENHEIGHT/2)-f.getHeight()/2);
-        f.setVisible(true);
+        this.setContentPane(p);
+        this.setAlwaysOnTop(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocation((SCREENWIDTH/2)-this.getWidth()/2, (SCREENHEIGHT/2)-this.getHeight()/2);
+        this.setVisible(true);
 
-        while(pointsToAdd==0){
-
+        while(x==0){
+            System.out.println("ignore this");
         }
-        f.setEnabled(false);
-        f.setVisible(false);
-        return pointsToAdd;
-
     }
 
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+        if(s.equals("b1")){
+            CounterClass.score+=pointsArray[0];
+            x=1;
+            System.out.println("Button1");
+            closeClass();
+        }else if(s.equals("b2")){
+            CounterClass.score+=pointsArray[1];
+            x=1;
+            System.out.println("Button2");
+            closeClass();
+        }else if(s.equals("b3")){
+            CounterClass.score+=pointsArray[3];
+            x=1;
+            System.out.println("Button3");
+            closeClass();
+        }else if(s.equals("quit")){
+            System.exit(0);
+        }
+    }
+
+    public void closeClass(){
+        this.dispose();
+    }
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,9 @@
 public class Runner {
 
     public static void main(String[] args) {
-        String[] test = {"Question", "Two", "Three", "Four", "Five"};
-        JFrameMaker j1 = new JFrameMaker(test);
+        String[] test = {"One", "Two", "Three", "Question"};
+        int[] array = {1,2,3,4};
+        JFrameMaker j1 = new JFrameMaker(test, array);
+        System.out.println("Here " + CounterClass.score);
     }
 }
